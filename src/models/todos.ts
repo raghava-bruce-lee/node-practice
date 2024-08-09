d21f33120fb96580da072fae90dfda18ef5ca47e
Raghavendra <57036150+raghava-bruce-lee@users.noreply.github.com>
2024-08-09 19:45:05 +0530

    add timestamps (#8)

--- src/models/todos.ts
+++ src/models/todos.ts
@@ -2,24 +2,27 @@ import { Schema, model } from 'mongoose';
 import { TODO_STATUS } from '../constants/todos';
 import constants from '../constants/common';
 
-const todoSchema = new Schema({
-  title: {
-    type: String,
-    required: true
+const todoSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    status: {
+      type: String,
+      default: TODO_STATUS.NOT_STARTED
+    },
+    userId: {
+      type: Schema.Types.ObjectId,
+      ref: constants.USER,
+      required: true
+    }
   },
-  description: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    default: TODO_STATUS.NOT_STARTED
-  },
-  userId: {
-    type: Schema.Types.ObjectId,
-    ref: constants.USER,
-    required: true
-  }
-});
+  { timestamps: true }
+);
 
 export const Todo = model(constants.TODO, todoSchema);
