1e0dea9e9808f9d7429aa2c08d168acd155541ef
Raghavendra <57036150+raghava-bruce-lee@users.noreply.github.com>
2024-08-11 14:41:29 +0530

    add login status endpoint (#9)

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { body } from 'express-validator';
 import { User } from '../models/users';
-import { signup, login } from '../controllers/auth';
+import { signup, login, getLoginStatus } from '../controllers/auth';
 import { AUTH_REQUEST } from '../constants/todos';
 import constants from '../constants/common';
+import { isAuth } from '../middlewares/is-auth';
 
 const router = Router();
 
@@ -29,5 +30,7 @@ router.post(
   [body(AUTH_REQUEST.email).isEmail(), body(AUTH_REQUEST.password).trim().isLength({ min: 5 })],
   login
 );
+
+router.get('/login-status', isAuth, getLoginStatus);
 
 export default router;
